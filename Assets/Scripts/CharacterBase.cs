57bbd6c06dafd1c26bd6eae203a441f485f07d6d
definetlynotadog <definetlynotadog@users.noreply.github.com>
2016-09-01 23:34:44 +0300

    Idle and run animations added for test character; Switched camera to orthographic mode; Basic movement implemented;

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -2,26 +2,62 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CharacterBase : MonoBehaviour {
+public class CharacterBase : MonoBehaviour
+{
+    protected Animator animator;
+    protected Rigidbody rigidBody;
 
-    protected SpriteRenderer spriteRenderer;
+    protected bool isMoving = false;
+    protected float northDir = -1;
+    protected float eastDir  = 0;
 
-    public List<Sprite> spriteIdleSouth     = new List<Sprite>();
-    public List<Sprite> spriteIdleNorth     = new List<Sprite>();
-    public List<Sprite> spriteIdleWest      = new List<Sprite>();
-    
-    public List<Sprite> spriteRunSouth      = new List<Sprite>();
-    public List<Sprite> spriteRunNorth      = new List<Sprite>();
-    public List<Sprite> spriteRunWest       = new List<Sprite>();
-    public List<Sprite> spriteRunNorthEast  = new List<Sprite>();
-    public List<Sprite> spriteRunSouthWest  = new List<Sprite>();
+    [SerializeField]
+    protected float movingSpeed;
 
-    void Start () {
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 	
-	void Update () {
-	
+	void Update ()
+    {
+        float verticalInput   = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");        
+
+        if (verticalInput != 0 || horizontalInput != 0)
+        {
+            northDir = Mathf.Sign(verticalInput) * (Mathf.Abs(verticalInput) > 0.1 ? 1 : 0);
+            eastDir = Mathf.Sign(horizontalInput) * (Mathf.Abs(horizontalInput) > 0.1 ? 1 : 0);
+
+            Vector3 velocity = rigidBody.velocity;
+            velocity.z = northDir;
+            velocity.x = eastDir;
+            rigidBody.velocity = velocity.normalized * movingSpeed;
+
+            isMoving = true;
+        }
+        else
+        {
+            rigidBody.velocity = Vector3.zero;
+
+            isMoving = false;
+        }
 	}
+
+    void FixedUpdate()
+    {
+        UpdateAnimations();
+    }
+
+    protected void UpdateAnimations()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("is_running", isMoving);
+            animator.SetFloat("move_speed", movingSpeed);
+            animator.SetFloat("north_dir", northDir);
+            animator.SetFloat("east_dir", eastDir);
+        }
+    }
 }
