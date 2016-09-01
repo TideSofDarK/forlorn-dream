e643ec005c52d7c4f4b1db8b63bdbc566cd2c253
TideSofDarK <tidesmain@gmail.com>
2016-09-01 22:41:11 +0500

    Initial commit

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterBase : MonoBehaviour {
+
+    protected SpriteRenderer spriteRenderer;
+
+    public List<Sprite> spriteIdleSouth     = new List<Sprite>();
+    public List<Sprite> spriteIdleNorth     = new List<Sprite>();
+    public List<Sprite> spriteIdleWest      = new List<Sprite>();
+    
+    public List<Sprite> spriteRunSouth      = new List<Sprite>();
+    public List<Sprite> spriteRunNorth      = new List<Sprite>();
+    public List<Sprite> spriteRunWest       = new List<Sprite>();
+    public List<Sprite> spriteRunNorthEast  = new List<Sprite>();
+    public List<Sprite> spriteRunSouthWest  = new List<Sprite>();
+
+    void Start () {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+    }
+	
+	void Update () {
+	
+	}
+}
