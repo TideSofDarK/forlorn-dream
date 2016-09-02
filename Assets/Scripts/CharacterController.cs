1d2016477c6e7d0817308be975868a1b268aa45e
TideSofDarK <tidesmain@gmail.com>
2016-09-02 19:36:37 +0500

    Testing floor tile; Billboard script; CharacterController empty script

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    //[SerializeField]
+    //protected Camera camera;
+
+	void Start () {
+	    
+	}
+	
+	void Update () {
+	    
+	}
+}
