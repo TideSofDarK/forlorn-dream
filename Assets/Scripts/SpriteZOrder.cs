f1e0df31d1bddd6045898a889ba7348e17a2dc41
TideSofDarK <tidesmain@gmail.com>
2016-09-05 03:02:40 +0500

    New testing scene; Initial Sprite Z Order script

---
+++ Assets/Scripts/SpriteZOrder.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteZOrder : MonoBehaviour {
+
+    [SerializeField]
+    protected SpriteRenderer spriteRenderer;
+
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	void Update () {
+        if (spriteRenderer)
+        {
+            spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.z * 100f) * -1;
+        }
+    }
+}
